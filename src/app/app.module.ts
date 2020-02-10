2ae3f39a6dd17082dbda419f1292dff85acf752a
seba1165 <scalderon@kunder.cl>
2020-02-10 11:57:44 -0300

    Config inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './components/components.module';
+
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    GotyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
+
+@NgModule({
+  declarations: [NavbarComponent, GraficoBarraHorizontalComponent],
+  exports: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class ComponentsModule { }
